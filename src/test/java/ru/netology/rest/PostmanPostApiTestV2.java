1c781a48eacb310bf2fbd66aa5ee0c8bc04c5480
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-20 19:28:36 +0300

    correct tests

--- src/test/java/ru/netology/rest/PostmanPostApiTestV2.java
+++ src/test/java/ru/netology/rest/PostmanPostApiTestV2.java
@@ -19,7 +19,7 @@ public class PostmanPostApiTestV2 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
